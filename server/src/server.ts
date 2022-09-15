af606d8421ff848af824ea7c6df3ef7f08c50122
VitorM3L0 <vitormelobatista@gmail.com>
2022-09-14 22:08:05 -0300

    Finalizando o backEnd.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,91 @@
-import express from "express"
+import express, { request, response } from "express"
+import { PrismaClient } from '@prisma/client'
+import cors from 'cors'
+import { convertHourStringToMinutes } from "./utils/convert-hour-string-to-minutes"
+import { convertMinutesToHourString } from "./utils/convert-minutes-to-hour-string"
 
 const app = express()
+app.use(express.json())
+app.use(cors({
+  origin: 'http://localhost:3333'
+}))
+const prisma = new PrismaClient({
+  log: ['query']
+})
 
-app.get('/ads', (request, response) => {
-  return response.json([
-    { id: 1, name: 'Anuncio1' },
-    { id: 2, name: 'Anuncio2' },
-    { id: 3, name: 'Anuncio3' },
-    { id: 4, name: 'Anuncio4' },
-  ])
+app.get('/games', async (request, response) => {
+  const games = await prisma.game.findMany({
+    include: {
+      _count: {
+        select: {
+          ads: true
+        }
+      }
+    }
+  })
+
+  return response.json(games)
+})
+
+app.post('/games/:id/ads', async (request, response) => {
+  const gameId = request.params.id;
+  const body: any = request.body
+
+
+  const ad = await prisma.ad.create({
+    data: {
+      gameId,
+      name: body.name,
+      weekDays: body.weekDays.join(','),
+      useVoiceChannel: body.useVoiceChannel,
+      yearsplaying: body.yearsplaying,
+      hourStart: convertHourStringToMinutes(body.hourStart),
+      hourEnd: convertHourStringToMinutes(body.hourEnd),
+      discord: body.discord,
+    }
+  })
+  return response.status(201).json(ad)
+})
+
+app.get('/games/:id/ads', async (request, response) => {
+  const gameId = request.params.id;
+  const ads = await prisma.ad.findMany({
+    select: {
+      id: true,
+      name: true,
+      weekDays: true,
+      useVoiceChannel: true,
+      yearsplaying: true,
+      hourStart: true,
+      hourEnd: true,
+    },
+    where: {
+      gameId,
+    },
+    orderBy: {
+      createdAt: 'desc'
+    }
+  })
+  return response.json(ads.map(ad => {
+    return {
+      ...ad,
+      weekDays: ad.weekDays.split(','),
+      hourStart: convertMinutesToHourString(ad.hourStart),
+      hourEnd: convertMinutesToHourString(ad.hourEnd)
+    }
+  }))
+})
+app.get('/ads/:id/discord', async (request, response) => {
+  const adId = request.params.id;
+  const ad = await prisma.ad.findUniqueOrThrow({
+    select: {
+      discord: true
+    }, where: {
+      id: adId
+    }
+  })
+  return response.json({
+    discord: ad.discord
+  })
 })
 app.listen(3333)  
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className='max-w-[1344px] mx-auto flex flex-col items-center my-20'>
       <img src={logoImg} />
-      <h1 className='text-6xl text-white font-black mt-20'>Seu <span className='bg-nlw-gradient text-transparent bg-clip-text'>duo</span> está aqui.</h1>
+      <h1 className='text-6xl text-white font-black mt-20'>Seu <span className='bg-nlw-gradient text-transparent bg-clip-text'>duo dev</span> está aqui.</h1>
 
       <div className='grid grid-cols-6 gap-6 mt-16'>
         <a href="" className='relative rounded-lg overflow-hidden'>
@@ -58,7 +58,7 @@ function App() {
         <div className='bg-[#2A2634] px-8 py-6 flex justify-between items-center'>
           <div>
             <strong className='text-2xl text-white font-black block'>Não encontrou seu duo?</strong>
-            <span className='text-zinc-400 block'>Publique um anuncio para encontrar novos players!</span>
+            <span className='text-zinc-400 block'>Publique um anúncio para encontrar novos devs!</span>
           </div>
           <button className='py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3'>
             <MagnifyingGlassPlus size={24} />
